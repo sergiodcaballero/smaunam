25929d2a1ac4afcb911c01fca5d3981e36746cc1
sergiodcaballero <sergiodcaballero@gmail.com>
2015-03-17 13:35:25 +0000

    _ agregue de bootstrap a la pantalla index.php _ realice la recuperacion de clave

--- consultar_orden.php
+++ consultar_orden.php
@@ -1,16 +1,84 @@
 <?php 
 ob_start();
-session_start() ?> 
+session_start(); ?> 
 <html>
-<head><title>Auto Gesti&oacute;n </title>
+<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css" />
+<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap-responsive.css" />
+<link rel="stylesheet" type="text/css" href="bootstrap/css/jquery.dialog2.css"/>
+ <link rel="stylesheet" type="text/css" href="bootstrap.min.css">
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+<script type="text/javascript" src="bootstrap/js/bootstrap.js"></script>
+<script type="text/javascript" src="bootstrap/js/bootbox.js"></script>
+<head>
+<title>Auto Gesti&oacute;n</title>
 <style type="text/css">
 <!--
 @import url("mm_restaurant1.css");
 -->
 </style>
+<style type="text/css">
+body{
+font-weight:normal;
+}
+</style>
+<script type="text/javascript">
+ 
+$(document).ready(function(e){
+			 $(".anular").click(function(evento){
+				 var num_orden = $(this).attr('id');
+				//  bootbox.alert("Hello world!", function() {
+              //  console.log("Alert Callback");
+            //});
+			bootbox.confirm(
+			"¿Esta seguro de anular la orden Nº "+num_orden+"?", 			
+			function(result) {
+				//var box=bootbox.dialog({
+					//	message: '</br> Se está enviando su petición de Anulación</br></br></br>'+
+					//	'<div class="progress progress-striped active">'+ 
+					//				'<div class="bar" style="width: 100%;"> ' +
+					//				'</div></br>',
+					//	closeButton: false,	
+         //  });
+				if (result==true){
+					
+		  //box.modal('show');
+					var url ="anulacion.php"; 
+					$.post(url,{orden:num_orden},function(valor,suceso){
+						//alert(valor);
+						if (suceso=='success'){
+							//box.modal('hide');
+								alert('La orden ha sido eliminada exitosamente');	
+								
+							}else{
+								//box.modal('hide');
+								alert('Intente más tarde');
+						}
+						 location.reload();
+					
+					});
+					
+					
+					}
+			}); 
+				// alert (num_orden);
+			//	 if (confirm("¿Esta seguro de anular la orden? Una vez que se anule la orden no se la puede utilizar."))
+		//	{
+			//	var url ="anulacion.php";
+				//$.post(url,{orden:num_orden},function(valor){
+					//alert(valor);
+					//});
+		//}
+				// alert('holaaaaaaaaaaaaaaaaaaaaaaaa');
+				// alert($(this).attr('id'));
+				});
+				});
+	
+	
+	
+</script>
 </head>
 <body>
-
+ 
 <table width="933" height="461" border="1">
 <tr>
 <th width="911" bgcolor="#99ccff"><div align="left"><font color='blue'><span class="logo"><img src="Image3.gif" alt="Logo" width="351" height="90" border="1" />AUTO GESTI&Oacute;N S.M.A.U.Na.M<br />
@@ -18,21 +86,26 @@ session_start() ?>
 </tr>
 <tr>
 <td height="169"><div align="center">
-<p>&nbsp;</p>
-<table width="796" border='1'>
-<caption style='font-size:16pt'>
- Consumo de Ordenes de Consulta expendidas por Auto Gesti&oacute;n
-<br>
-</caption>
+
+<br/>
+<h4>
+ Consumo de Ordenes de Consulta por Auto Gesti&oacute;n
+<br/>
+</h4>
+<br/><br/>
+<table width="94%" class='table table-striped' style="width:92%;">
+<thead>
 <tr>
-<th width="51" height="40">Nº Orden </th>
-<th width="64"><p>Nº Afiliado</p>  </th>
-<th width="152">Nombre</th>
-<th width="153">Fecha de Emisión</th>
-<th width="38">Hora</th>
-<th width="60">C&oacute;digo</th>
-<th width="150">Descripci&oacute;n</th>
-<th width="76">Coseguro</th>
+<th width="9%" ><center>Nº Orden </center></th>
+<th width="9%" ><center>Nº Afiliado</center></th>
+<th width="9%" ><center>Nombre</center></th>
+<th width="15%"><center>Fecha de Emisión</center></th>
+<th width="7%" ><center>Hora</center></th>
+<th width="9%"><center>C&oacute;digo</center></th>
+<th width="13%" ><center>Descripci&oacute;n</center></th>
+<th width="11%" ><center>Coseguro</center></th>
+<th width="18%" ></th>
+</tr></thead><tbody>
 <?php
  //Establecimiento de la conexi&oacute;n 
 require_once('connections/honorarios.php'); 
@@ -53,23 +126,52 @@ and detalle_orden_medica.codigo = nomenclador.codigo
 AND ordenes_medicas.documento =".$N_Afiliado." AND ordenes_medicas.Fecha_emision BETWEEN  '".$fecha_desde."' and '".$Fecha."' and  Forma_Pago <> 'ANUL'";
 $resultado = mysql_query($consulta);
 $Cantidad_Filas = mysql_num_rows($resultado);
-echo "<br /> Cantidad de Filas Encontradas :$Cantidad_Filas <br />\n";
+//echo "<br /> Cantidad de Filas Encontradas :$Cantidad_Filas <br />\n";
 if ($Cantidad_Filas < 1): 
 	echo "<br /> No se encontraron Cosumos para el Afiliado<br />\n";
 else: 
+//date_default_timezone_set('America/Argentina/Buenos_Aires'); 
 	//Recorrido del cursor de fila en fila
 	while ($fila = mysql_fetch_array($resultado)){
 	//Proceso de cada una de las filas
+	
 	echo "<tr>"; 
 	echo "<td>", $fila['Numero'], "</td>";
 	echo "<td>", $fila['Documento'], "</td>";
 	echo "<td>", $fila['Afiliado'], "</td>";
-	echo "<td>", $fila['Fecha_emision'], "</td>";
+	echo "<td><center>", $fila['Fecha_emision'], "</center></td>";
 	echo "<td>", $fila['hora'], "</td>";
 	echo "<td>", $fila['codigo'], "</td>";
 	echo "<td>", $fila['descripcion'], "</td>";
 	echo "<td>", $fila['coseguro'], "</td>";
-	echo "</tr>\n"; 
+	if ($fila['Fecha_emision']==date("Y-m-d")){
+		echo "<td> 
+	<a class='btn anular btn-danger' id=",$fila['Numero'],">Anular Orden</a>
+				</td>";
+		}else{
+		 $nuevafecha = date('Y-m-d', strtotime($fila['Fecha_emision']) + 86400); 
+		 if ($nuevafecha==date("Y-m-d")){
+			 $hora =  date("h:i:s", time());
+			 $hora1 =  strtotime($hora);
+			 $hora2 =  strtotime($fila['hora']);
+			 //echo $hora;
+			 if ($hora2>$hora1){
+				 	echo "<td> 
+	<a class='btn anular btn-danger' id=",$fila['Numero'],">Anular Orden</a>
+				</td>";
+				 }else{
+					  echo "<td> 
+	<a class='btn btn-danger disabled' >Anular Orden</a>
+				</td>";
+				 }
+			 
+			 }else{
+				 echo "<td> 
+	<a class='btn btn-danger disabled' >Anular Orden</a>
+				</td>";
+			 }
+	}
+	echo "</tr>\n "; 
 	
 	} 
 	
@@ -83,8 +185,9 @@ mysql_close();
 
 
 ?>
-</tr>
+</tbody>
 </table>
+
 <p>&nbsp;</p>
 <table width="217" border="0">
   <tr>
@@ -106,5 +209,7 @@ mysql_close();
 <p><br /> 
 </p>
 <hr />
+
+
 </body>
 </html>
--- index.php
+++ index.php
@@ -11,8 +11,73 @@
 <!-- Copyright 2005 Macromedia, Inc. All rights reserved. -->
 <title>Auto Gesti&oacute;n  - P&aacute;gina principal</title>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<link rel="stylesheet" href="mm_restaurant1.css" type="text/css" />
+<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css" />
+<link href="estilos/mis_estilos.css" rel="stylesheet" type="text/css" />
+<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap-responsive.css" />
+
+
+
+
+<script type="text/javascript" src="ScriptLibrary/jquery-latest.pack.js"></script>
+<script type="text/javascript" src="bootstrap/js/bootstrap.js"></script>
+<script type="text/javascript" src="bootstrap/js/bootbox.js"></script>
+
+<script type="text/javascript" src="bootstrap/js/jquery.md5.min.js"></script>
 <script type="text/JavaScript">
+
+$(document).ready(function(e){
+
+	$(".anular").click(function(evento){
+		
+		});
+		
+		$(".confirmar").click(function(evento){
+			var mail = $(".mail").val();
+			if (mail==''){
+				alert("Ingrese su Correo!");
+			}else{
+				var verificar_email = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+				if (verificar_email.test(mail)==false) {
+						alert('El correo ingresado no es válido');
+				}else{	
+				
+				var box=bootbox.dialog({
+						message: '</br> Enviando mail..</br></br>'+
+						'<div class="progress progress-striped active">'+ 
+									'<div class="bar" style="width: 100%;"> ' +
+									'</div></br>',
+						closeButton: false,	
+           });box.modal('show');
+				$.ajax({
+    url: 'recuperar_clave.php',
+    data: {
+        mail: mail
+    },
+    type: 'POST',
+    dataType: 'json',
+    success: function(datos){
+		$('#myModal').modal('hide')
+		box.modal('hide');
+		if (datos.respuesta=='NO'){
+			alert('El correo ingresado no existe');
+		}else if(datos.respuesta=='SI'){
+			
+		 	alert('El correo ha sido enviado exitosamente!');
+		}else{
+			//$('#myModal').modal('hide')
+			alert('No se ha podido enviar el correo. Intente más tarde');
+		}
+     	//alert(JSON.stringify(datos, null, 4));
+       
+    }
+});
+					
+				}
+			}
+		});
+	});
+	
+	
 <!--
 function MM_findObj(n, d) { //v4.01
   var p,i,x;  if(!d) d=document; if((p=n.indexOf("?"))>0&&parent.frames.length) {
@@ -39,73 +104,94 @@ function MM_validateForm() { //v4.0
 }
 //-->
 </script>
+<style type="text/css">
+	.form-horizontal .control-label{
+		width:300px;}
+	
+</style>
 </head>
-<body bgcolor="#0066cc">
-
-<table width="100%" border="0" cellspacing="0" cellpadding="0">
-	<tr bgcolor="#99ccff">
-	<td height="60" nowrap="nowrap" bgcolor="#99ccff" class="logo"><img src="Image3.gif" width="351" height="90" border="1" /> AUTO GESTI&Oacute;N S.M.A.U.Na.M</td>
-	</tr>
-
-
-	<tr bgcolor="#ffffff">
-	<td width="75" height="334" valign="top">
-	<form name='datos' action='verificadatos.php' method='post' >
-      <p align="center">&nbsp;</p>
-      
-        <div align="center">
-          <table width="681" height="103" border="0">
-            <tr>
-              <td width="348" bordercolor="1"><div align="right" class="letra">Ingrese el Nro. de Afiliado (sin barra /)  </div></td>
-              <td width="315" bordercolor="1"><label>
-                <div align="left">
-                  <input name="N_Afiliado" type="text" maxlength="10" />
-                  </div>
-              </label></td>
-            </tr>
-            <tr>
-              <td bordercolor="1"><div align="right" class="letra">Ingrese el Nro. de Documento: </div></td>
-              <td bordercolor="1"><label>
-                
-                <div align="left">
-                  <input name="Nro_Doc" type="text" maxlength="8" />
-                  </label>
-              </div></td>
-            </tr>
-            <tr>
-              <td bordercolor="1"><div align="right"><span class="letra">Ingrese la Contrase&ntilde;a</span>: </div></td>
-              <td bordercolor="1"><div align="left">
-                <input name="password" type="password" maxlength="10" />
-              </div></td>
-            </tr>
-            <tr>
-              <td align="center" bordercolor="1"><label>
-                <input name="submit" type="submit" onclick="MM_validateForm('N_Afiliado','','RisNum','Nro_Doc','','RisNum');return document.MM_returnValue" value="Ingresar" />
-                
-              </label></td>
-              <td align="center" bordercolor="1"><a href="http://www.smaunam.com.ar" style="margin-left:2%;">
-	  <input type="button" value="Volver" name="submit" /></a></td>
-            </tr>
-          </table>
+<body>
+<div class="container">
+	<div class="contenido">
+    
+  <div class="row-fluid">
+    <div class="span12">
+		<div class="encabezado">
+        	<div class="row-fluid">
+            	<div class="span4">
+           	    <img src="images/Logo.gif" class="img-rounded" style="margin-left:1%" /> 
+                </div>
+                <div class="span8">
+                	<div class="titulo">
+                    	<h3>AUTOGESTI&Oacute;N S.M.A.U.Na.M.</h3>
+                    </div>
+                </div>
+               
+            </div>
+        	  </div>
+    </div>
+    <div class="span12">
+    	<div class="contenido_base">
+       	<form name='datos' action='verificadatos.php' method='post' class="form-horizontal" >
+       	  <div class="control-group">
+       	    <label class="control-label" for="N_Afiliado" >Ingrese el Nro. de Afiliado (sin barra /)&nbsp;</label>
+            <div class="controls">
+           	  <input type="text" name="N_Afiliado" id="N_Afiliado" maxlength="10"/>
+            </div>
+          </div>
+          <div class="control-group">
+       	    <label class="control-label" for="Nro_Doc" >Ingrese el Nro. de Documento:&nbsp;</label>
+            <div class="controls">
+           	  <input type="text" name="Nro_Doc" id="Nro_Doc" maxlength="8"/>
+            </div>
+          </div>
+          <div class="control-group">
+       	    <label class="control-label" for="password" >Ingrese la Contraseña:&nbsp;</label>
+            <div class="controls">
+           	  <input type="password" name="password" id="password" maxlength="10"/>
+            </div>
+          </div>
+          <div class="control-group">
+          <center>
+          	<button type="submit" class="btn btn-success" onclick="MM_validateForm('N_Afiliado','','RisNum','Nro_Doc','','RisNum');return document.MM_returnValue">Ingresar</button>
+            <a class="btn" href="http://www.smaunam.com.ar" style="margin-left:2%;">Volver</a> 
+            <a href="#myModal" role="button" class="btn btn-primary" data-toggle="modal"  style="margin-left:2%;">Recuperar Contraseña</a> 
+          </center>
+          </div>
+        </form>
+        <div class="alert alert-info" style="margin-right:20%;margin-top:2%; margin-left:-10%;padding-top:1%">
+        	<strong>*Señor Afiliado:</strong><br/>
+            - Para poder ingresar al sistema de auto Gestión deberá enviar una solicitud <strong>Alta Usuario.</strong><br/>
+            - Para descargar el manual de Ayuda, seleccione 
+            <a target="_blank" href="http://www.smaunam.com.ar/wp-content/uploads/2015/01/Manual-Sistema-Autogestión.pdf" class="btn ">Descargar</a>
+           
         </div>
-        <p align="center">
-        <label></label></p>
-        <table width="679" border="1">
-          <tr>
-            <td class="letra">* <strong>Se&ntilde;or Afiliado: </strong>Para poder ingresar al sistema de auto Gesti&oacute;n deber&aacute; ingresar los datos de N&ordm; de afiliado y DNI de la persona a utilizar el sistema.  </td>
-          </tr>
-        </table>
-	</form>
-	  <p style="margin-left:4%;">
-	    <label></label>
-	  Para descargar el manual de Ayuda, seleccione el bot&oacute;n <strong>&quot;Descargar&quot;</strong> <a target="_blank" href="http://www.smaunam.com.ar/wp-content/uploads/2015/01/Manual-Sistema-Autogestión.pdf" style="margin-left:2%;">
-	  <input type="button" value="Descargar" name="submit" /></a></p>
-	</td>
-	</tr>
+        </div>
+    </div>
+    <div class="span12">
+                	
+    </div>
+  </div>
+</div>
+</div>
+<div id="myModal" class="modal hide fade " tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" style="width:40%;">
+  <div class="modal-header">
+    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+    <h3 id="myModalLabel">Recuperar Contraseña</h3>
+  </div>
+  <div class="modal-body">
+    <p>Ingrese su Correo Electrónico</p>
+    <div class="control-group">
+       	  <div class="controls">
+           	  <input name="mail" id="mail" type="email" class="mail form-control"/>
+            </div>
+          </div>
+  </div>
+  <div class="modal-footer">
+     <button class="btn btn-success reenvio_clave confirmar" >Aceptar</button>
+     <button class="btn"  data-dismiss="modal" aria-hidden="true">Cancelar</button>
+  </div>
+</div>
 
-	<tr bgcolor="#ffffff">
-	<td bgcolor="#0000FF"><img src="mm_spacer.gif" alt="" width="1" height="1" border="0" /></td>
-	</tr>
-</table>
 </body>
 </html>
